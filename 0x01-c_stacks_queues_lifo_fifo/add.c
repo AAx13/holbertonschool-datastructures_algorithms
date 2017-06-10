2a5fced4a507d37960849d6b1b8b04077da6cf38
DamianAli <damian.ali33@gmail.com>
2017-06-10 10:05:44 -0700

    Testing null case removal against hbtn checks.

--- 0x01-c_stacks_queues_lifo_fifo/add.c
+++ 0x01-c_stacks_queues_lifo_fifo/add.c
@@ -13,7 +13,7 @@ void add(stack_t **stack, unsigned int line_number)
 {
 	stack_t *prev_node, *head;
 
-	if (!*stack || (*stack && (*stack)->next == NULL))
+	if (*stack && (*stack)->next == NULL)
 	{
 		printf("L%d: can't add, stack too short\n", line_number);
 		exit(EXIT_FAILURE);
