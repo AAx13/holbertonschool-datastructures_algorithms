0e6e294afe363e191bbba5a021661dd0d4124d28
DamianAli <damian.ali33@gmail.com>
2017-06-10 16:10:21 -0700

    Fixed bug in syntax for sub.c. running against hbtn checks.

--- 0x01-c_stacks_queues_lifo_fifo/sub.c
+++ 0x01-c_stacks_queues_lifo_fifo/sub.c
@@ -11,7 +11,7 @@
  */
 void op_sub(stack_t **stack, unsigned int line_number)
 {
-	stack_ *head;
+	stack_t *head;
 
 	head = *stack;
 	if (!head || head->next == NULL)
@@ -20,7 +20,7 @@ void op_sub(stack_t **stack, unsigned int line_number)
 		exit(EXIT_FAILURE);
 	}
 
-	head->n -= head->next->n;
+	head->next->n -= head->n;
 	head->next->prev = NULL;
 	*stack = head->next;
 	free(head);
