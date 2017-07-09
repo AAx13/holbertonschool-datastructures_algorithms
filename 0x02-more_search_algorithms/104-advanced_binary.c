62adaaef02ab358fe4d494693ca50090983dd612
DamianAli <damian.ali33@gmail.com>
2017-07-08 22:19:09 -0700

    added docs and removed unnecessary if statement. testing against hbtn checks.

--- 0x02-more_search_algorithms/104-advanced_binary.c
+++ 0x02-more_search_algorithms/104-advanced_binary.c
@@ -1,5 +1,15 @@
 #include "search_algos.h"
 
+/**
+ * advanced_binary - searches for a value in an array of integers.
+ * return the index of the first value in the array
+ * (if this value appears more than once in the array).
+ * @array: Is a pointer to the first element of the array to search.
+ * @size:  s the number of elements in array.
+ * @value: Value is the value to search for.
+ *
+ * Return: Index where value exists or -1.
+ */
 int advanced_binary(int *array, size_t size, int value)
 {
 	size_t mid, left, right, print;
@@ -24,8 +34,6 @@ int advanced_binary(int *array, size_t size, int value)
 				}
 				print++;
 			}
-			if (left == right && array[left] != value)
-				return (-1);
 
 			if (value == array[mid] && array[mid - 1] != value)
 				return (mid);
