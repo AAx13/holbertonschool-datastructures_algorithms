8bcf755a2acd0cc95a1994a4f85c7ec473682d29
DamianAli <damian.ali33@gmail.com>
2017-07-13 20:47:41 -0700

    testing value > parent->n functionality for task 1 against hbtn checks.

--- 0x03-binary_trees/1-binary_tree_insert_left.c
+++ 0x03-binary_trees/1-binary_tree_insert_left.c
@@ -12,7 +12,7 @@ binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 	binary_tree_t *new_node, *tmp_node;
 
 	new_node = malloc(sizeof(binary_tree_t));
-	if (!new_node)
+	if (!new_node || parent->n < value)
 	{
 		return (NULL);
 	}
