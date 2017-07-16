df5d2a71f38dd733ba6323c3165f5895135f2cc7
DamianAli <damian.ali33@gmail.com>
2017-07-16 16:02:30 -0700

    testing task 14 against hbtn checks.

---
+++ 0x03-binary_trees/14-binary_tree_balance.c
@@ -0,0 +1,61 @@
+#include "binary_trees.h"
+#include <stdio.h>
+
+/**
+ * binary_tree_height - measures the height of a binary tree.
+ * @tree: A pointer to a node in a binary tree.
+ *
+ * Return: Height or 0 if NULL.
+ */
+size_t binary_tree_height(const binary_tree_t *tree)
+{
+	size_t left, right;
+
+	if (!tree || (!tree->left && !tree->right))
+	{
+		return (0);
+	}
+
+	left = binary_tree_height(tree->left);
+	right = binary_tree_height(tree->right);
+
+	if (left > right)
+	{
+		return (left + 1);
+	}
+	else
+	{
+		return (right + 1);
+	}
+}
+
+/**
+ * binary-tree_balance - measures the balance factor of a binary tree
+ * to measure the balance factor of.
+ * @tree: A pointer to the root node of the tree.
+ *
+ * Return: Balance factor of the tree (int).
+ */
+int binary_tree_balance(const binary_tree_t *tree)
+{
+	int left, right;
+
+	if (!tree || (!tree->right && !tree->left))
+	{
+		return (0);
+	}
+
+	left = binary_tree_height(tree->left);
+	right = binary_tree_height(tree->right);
+
+	if (left == 0 && right == 0)
+	{
+		return (-1);
+	}
+	else
+	{
+		return (left - right);
+	}
+
+	return (0);
+}
