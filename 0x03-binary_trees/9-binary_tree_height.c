d2974d2d686206c55eb205c1bf98d7fe7a1fe9ec
DamianAli <damian.ali33@gmail.com>
2017-07-15 20:40:38 -0700

    testing task 9 against hbtn checks.

--- 0x03-binary_trees/9-binary_tree_height.c
+++ 0x03-binary_trees/9-binary_tree_height.c
@@ -13,12 +13,16 @@ size_t binary_tree_height(const binary_tree_t *tree)
 		return (0);
 	}
 
-	if (tree->left)
+	if (tree->left && tree->right)
+	{
+		return (binary_tree_height(tree->left)
+			+ binary_tree_height(tree->right));
+	}
+	else if (tree->left && !tree->right)
 	{
 		return (1 + binary_tree_height(tree->left));
 	}
-
-	if (tree->right)
+	else if (tree->right && !tree->left)
 	{
 		return (1 + binary_tree_height(tree->right));
 	}
