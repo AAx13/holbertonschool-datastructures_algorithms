bcbf4a288c8493aafe782082f1d1c72709f86f1c
DamianAli <damian.ali33@gmail.com>
2017-07-15 20:02:35 -0700

    testing task 10 againt hbtn checks.

---
+++ 0x03-binary_trees/10-binary_tree_depth.c
@@ -0,0 +1,22 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_depth - measures the depth of a node in a binary tree.
+ * @tree: A pointer to a node in a binary tree.
+ *
+ * Return: Depth or 0 if NULL.
+ */
+size_t binary_tree_depth(const binary_tree_t *node)
+{
+	if (!node)
+	{
+		return (0);
+	}
+
+	if (node->parent)
+	{
+		return (1 + binary_tree_depth(node->parent));
+	}
+
+	return (0);
+}
