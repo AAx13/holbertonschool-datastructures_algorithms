0733a4265f3b0574d257913c3ed8f6e78dee5cad
DamianAli <damian.ali33@gmail.com>
2017-07-15 19:05:34 -0700

    finished with task 8 for now. 1 red check left.

--- 0x03-binary_trees/8-binary_tree_postorder.c
+++ 0x03-binary_trees/8-binary_tree_postorder.c
@@ -1,5 +1,13 @@
 #include "binary_trees.h"
 
+/**
+ * binary_tree_postorder - goes through a binary tree
+ * using post-order traversal.
+ * @tree:A node in a binary tree.
+ * @func: Function pointer to one that prints the value of a node.
+ *
+ * Return: Void.
+ */
 void binary_tree_postorder(const binary_tree_t *tree, void (*func)(int))
 {
 	if (!tree)
