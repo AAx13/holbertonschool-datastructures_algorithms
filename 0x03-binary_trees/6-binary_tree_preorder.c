8cedbb3406f8f7f313001eb3b975b9a051630bd6
DamianAli <damian.ali33@gmail.com>
2017-07-15 18:19:37 -0700

    testing task 6 against htbn checks.

---
+++ 0x03-binary_trees/6-binary_tree_preorder.c
@@ -0,0 +1,23 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_preorder - goes through a binary tree using pre-order traversal.
+ * @tree: A node in a binary tree.
+ * @func: Punction pointer to function that prints the value of a node.
+ *
+ * Return: Void.
+ */
+void binary_tree_preorder(const binary_tree_t *tree, void (*func)(int))
+{
+	func(tree->n);
+
+	if (tree->left)
+	{
+		binary_tree_preorder(tree->left, func);
+	}
+
+	if (tree->right)
+	{
+		binary_tree_preorder(tree->right, func);
+	}
+}
