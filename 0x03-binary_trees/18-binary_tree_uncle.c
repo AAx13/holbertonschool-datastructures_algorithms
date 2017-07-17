4bc3fb1ca4030fdca40bcaee81b2cb37a5d9eeba
DamianAli <damian.ali33@gmail.com>
2017-07-16 20:24:39 -0700

    running task 18 against hbtn checks. changed doc on task 17.

--- 0x03-binary_trees/18-binary_tree_uncle.c
+++ 0x03-binary_trees/18-binary_tree_uncle.c
@@ -25,3 +25,19 @@ binary_tree_t *binary_tree_sibling(binary_tree_t *node)
 
 	return (NULL);
 }
+
+/**
+ * binary_tree_uncle - finds the uncle of a node.
+ * @node:  A pointer to a binary tree node.
+ *
+ * Return: The uncle of the node passed. Or NULL if failed.
+ */
+binary_tree_t *binary_tree_uncle(binary_tree_t *node)
+{
+	if (!node || !node->parent || !node->parent->parent)
+	{
+		return (NULL);
+	}
+
+	return (binary_tree_sibling(node->parent));
+}
