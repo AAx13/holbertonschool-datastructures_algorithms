59f702ebceca06958f3673e3316f109e5fed7196
DamianAli <damian.ali33@gmail.com>
2017-07-16 20:12:57 -0700

    testing task 17 against htbn checks.

---
+++ 0x03-binary_trees/17-binary_tree_sibling.c
@@ -0,0 +1,28 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_sibling - finds the sibling of a node.
+ * @node:  A pointer to a binary tree node.
+ *
+ * Return: Returns the value of the sibling to the node passed.
+ * Or NULL if failed.
+ */
+binary_tree_t *binary_tree_sibling(binary_tree_t *node)
+{
+	if (!node || !node->parent)
+	{
+		return (NULL);
+	}
+
+	if (node->parent->left == node)
+	{
+		return (node->parent->right);
+	}
+
+	if (node->parent->right == node)
+	{
+		return (node->parent->left);
+	}
+
+	return (NULL);
+}
